07547d1910372f716e25bc474ed8cc08dea75327
Lulu <140290494+kirakiraminimalist@users.noreply.github.com>
2026-08-13 23:20:08 -0700

    feat: show idea inbox on home page

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,9 +1,5 @@
-import { PlanInput } from '@/components/app/plan-input'
+import { IdeaBoard } from '@/components/app/idea-board'
 
 export default function AppPage() {
-  return (
-    <div className="container mx-auto p-4 space-y-4">
-      <PlanInput />
-    </div>
-  )
+  return <IdeaBoard />
 }
